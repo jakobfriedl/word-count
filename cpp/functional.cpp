4b6e7d485b7882f057838219dc25f547b5dc4de0
Philipp Haider <if20b097@technikum-wien.at>
2022-11-13 21:09:39 +0100

    rewrote some functionality and .txt files

--- cpp/functional.cpp
+++ cpp/functional.cpp
@@ -12,11 +12,11 @@ using namespace std;
 namespace fs = std::filesystem;
 
 // get all Files in a Directory
-vector<fs::path> getFiles(fs::path path)
+vector<fs::path> getFiles(fs::path path, string extension)
 {
     vector<fs::path> files;
-    ranges::copy_if(fs::directory_iterator(path), back_inserter(files), [](const auto &entry)
-                    { return entry.is_regular_file(); });
+    ranges::copy_if(fs::directory_iterator(path), back_inserter(files), [&extension](const auto &entry)
+                    { return entry.is_regular_file() && entry.path().extension() == extension; });
     return files;
 }
 
@@ -30,15 +30,15 @@ vector<fs::path> getSubdirectories(fs::path path)
 }
 
 // get all Files in a Directory and all Subdirectories
-vector<fs::path> getAllFiles(fs::path path)
+vector<fs::path> getAllFiles(fs::path path, string extension)
 {
     vector<fs::path> files;
     vector<fs::path> subdirectories = getSubdirectories(path);
-    ranges::for_each(subdirectories, [&files](const auto &subdirectory)
+    ranges::for_each(subdirectories, [&files, &extension](const auto &subdirectory)
                      {
-        vector<fs::path> subdirectoryFiles = getAllFiles(subdirectory);
+        vector<fs::path> subdirectoryFiles = getAllFiles(subdirectory, extension);
         ranges::copy(subdirectoryFiles, back_inserter(files)); });
-    vector<fs::path> directoryFiles = getFiles(path);
+    vector<fs::path> directoryFiles = getFiles(path, extension);
     ranges::copy(directoryFiles, back_inserter(files));
     return files;
 }
@@ -67,10 +67,10 @@ vector<string> getStrings(fs::path path)
 }
 
 // get all Strings in a File and all Files in a Directory and all Subdirectories
-vector<string> getAllStrings(fs::path path)
+vector<string> getAllStrings(fs::path path, string extension)
 {
     vector<string> strings;
-    vector<fs::path> files = getAllFiles(path);
+    vector<fs::path> files = getAllFiles(path, extension);
     ranges::for_each(files, [&strings](const auto &file)
                      {
         vector<string> fileStrings = getStrings(file);
@@ -79,10 +79,10 @@ vector<string> getAllStrings(fs::path path)
 }
 
 // count all Strings in a File and all Files in a Directory and all Subdirectories
-map<string, int> countStrings(fs::path path)
+map<string, int> countStrings(fs::path path, string extension)
 {
     map<string, int> stringCount;
-    vector<string> strings = getAllStrings(path);
+    vector<string> strings = getAllStrings(path, extension);
     ranges::for_each(strings, [&stringCount](const auto &string)
                      {
         if (stringCount.find(string) == stringCount.end()) {
@@ -95,9 +95,9 @@ map<string, int> countStrings(fs::path path)
 }
 
 // write the Frequency and Strings sorted reverse alphabetically to a File
-void writeStrings(fs::path path)
+void writeStrings(fs::path path, string extension)
 {
-    map<string, int> stringCount = countStrings(path);
+    map<string, int> stringCount = countStrings(path, extension);
     multiset<pair<int, string>, greater<pair<int, string>>> stringFrequency;
     ranges::for_each(stringCount, [&stringFrequency](const auto &string)
                      { stringFrequency.insert(make_pair(string.second, string.first)); });
@@ -109,22 +109,25 @@ void writeStrings(fs::path path)
 // get path from command line
 int main(int argc, char *argv[])
 {
-
     string path;
-    if (argc == 2)
+    string extension;
+    if (argc == 3)
     {
         path = argv[1];
+        extension = argv[2];
     }
     else
     {
         cout << "Please enter a path: ";
         cin >> path;
+        cout << "Please enter a file extension: ";
+        cin >> extension;
     }
 
     // Write the Output to a File
     // start the timer
     auto start = chrono::high_resolution_clock::now();
-    writeStrings(path);
+    writeStrings(path, extension);
     // end the timer
     auto end = chrono::high_resolution_clock::now();
     // calculate the time
