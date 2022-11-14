4ca42743fa0cd45cbc218e469f8648bee76735c8
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2022-11-14 11:34:36 +0100

    Fix python implementation

--- wordcount.py
+++ wordcount.py
@@ -2,11 +2,14 @@ import os
 from pathlib import Path
 import sys
 import time
+from collections import Counter
+import regex as re
 
-def print_dict(d):
-    for k, v in d.items():
-        print(f"{k} -> {v}")
-
+def write_dict(d):
+    with open(r"output.txt", "w+") as f: 
+        for words, count in d.most_common():
+            if(words != ' ' and words != ''):
+                f.write(f"{count} {words}\n")
 
 if (len(sys.argv) < 3):
     print("Usage: python wordcount.py <folder> <extension>")
@@ -16,21 +19,18 @@ if (len(sys.argv) < 3):
 folder = sys.argv[1]
 ext = sys.argv[2]
 
-word_frequency = {}
+word_frequency = Counter()
 
 start = time.time()
 
 files = lambda x: [os.path.join('./', file) for file in Path(x).rglob('*' + ext)] 
 open_files = lambda x: [open(file, 'r') for file in files(x)]
 file_contents = lambda x: [file.read().replace('\n', ' ') for file in open_files(x)]
-words = lambda x: [word.lower() for content in file_contents(x) for word in content.split(' ') if word != '']
-inc = lambda x: x + 1; 
-word_count = lambda x: [word_frequency.update({word: inc(word_frequency.get(word, 0))}) for word in words(x)]
-
-# word_count = lambda x: [word_frequency.update({word: word_frequency.get(word, 0)+1}) for word in [word.lower() for content in [file.read().replace('\n', ' ') for file in [open(file, 'r') for file in [os.path.join('./', file) for file in Path(x).rglob('*' + ext)] ]] for word in content.split(' ') if word != '']]
+words = lambda x: [re.sub(r"[^a-z0-9 ]", "", word.lower()) for content in file_contents(x) for word in content.split(' ') if word != '' and word != ' ']
+word_count = lambda x: word_frequency.update(sorted(words(x), reverse=True))
 
 word_count(folder)
 
-print_dict(word_frequency)
+write_dict(word_frequency)
 
 print(time.time() - start)
